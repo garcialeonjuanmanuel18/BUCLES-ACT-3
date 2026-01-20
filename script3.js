28069f5c33a7b1fd99a594f79832d3a80ece4d5d
Juan Manuel García León <garcialeonjuanmanuel18@gmail.com>
2026-01-20 20:22:53 +0100

    script5.js

--- script3.js
+++ script3.js
@@ -1,1 +1,12 @@
-const notas=[0, 1, 32, 20, 8, 12];
+const numeros=[0, 1, 32, 20, 8, 12];
+
+let parmayor=0;
+
+for(i=0; i<numeros.length; i++){
+if(numeros[i]>10 && numeros[i]%2){
+    parmayor++;}
+}
+
+
+console.log(numeros);
+console.log("los números mayores de 10 y pares son: "+parmayor);
